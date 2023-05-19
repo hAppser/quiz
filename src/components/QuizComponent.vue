6f59485421c88a0e35d9a6db4afc662ca4d87340
hAppser <vip313z@ukr.net>
2023-05-19 12:27:34 +0300

    fixed some quiz error

--- src/components/QuizComponent.vue
+++ src/components/QuizComponent.vue
@@ -1,36 +1,56 @@
 <template>
   <div>
-    <h2>{{ currentQuestion.question }}</h2>
+    <h1>{{ currentQuestion.text }}</h1>
     <ul>
       <li v-for="answer in currentQuestion.answers" :key="answer.id">
-        <input type="radio" :value="answer.id" v-model="selectedAnswerId" />
-        {{ answer.text }}
+        <label>
+          <input
+            type="radio"
+            :value="answer.id"
+            v-model="selectedAnswer"
+            :disabled="currentQuestion.selectedAnswer !== null"
+            @change="handleAnswerSelection"
+          />
+          {{ answer.text }}
+        </label>
       </li>
     </ul>
-    <button @click="saveAnswer">Next</button>
+    <div v-if="quizCompleted">
+      <h2>Quiz Completed!</h2>
+    </div>
   </div>
 </template>
 
 <script>
-import { defineComponent, ref, computed } from "vue";
-import { useQuizStore } from "store/quizStore";
-export default defineComponent({
+import { ref } from "vue";
+import { useQuizStore } from "../../store/quizStore";
+
+export default {
   setup() {
     const quizStore = useQuizStore();
-    const selectedOption = ref("");
 
-    const currentQuestion = computed(() => quizStore.getCurrentQuestion());
+    const selectedAnswer = ref(null);
 
-    const submitAnswer = () => {
-      quizStore.submitAnswer(selectedOption.value);
-      selectedOption.value = "";
+    const handleAnswerSelection = (answerId) => {
+      quizStore.selectedAnswer(answerId);
+      if (quizStore.currentQuestion.selectedAnswer !== null) {
+        quizStore.nextQuestion();
+      }
     };
-
     return {
-      currentQuestion,
-      selectedOption,
-      submitAnswer,
+      currentQuestion: quizStore.currentQuestion,
+      selectedAnswer,
+      questions: quizStore.questions,
+      score: quizStore.score,
+      quizCompleted:
+        quizStore.currentQuestionIndex === quizStore.questions.length,
+      handleAnswerSelection,
     };
   },
-});
+};
 </script>
+<style>
+ul {
+  list-style-type: none;
+}
+</style>
